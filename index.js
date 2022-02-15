c6cb2a5a163740a0d84135fe7f29e680895bff51
azwar <azwar.akbar@gmail.com>
2022-02-15 18:06:17 +0700

    add user routes

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express()
 const port = 3000
 const user = require('./api/user')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
 
 app.use('/user', user)
+app.get('/', (req, res) => {
+  res.send('Welcome!')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
