b68a74c42955722cc3bd6f429e87624af5b0ccb6
azwar <azwar.akbar@gmail.com>
2022-02-16 17:41:09 +0700

    code refactor

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const { sendMailQueue } = require('./src/queues/greetings.ques')
 const app = express()
 const port = 3000
-const user = require('./api/user')
+const user = require('./src/routes/user')
 
 app.use(express.json())
 
@@ -9,7 +10,6 @@ app.use('/user', user)
 app.get('/', (req, res) => {
   res.send('Welcome!')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
