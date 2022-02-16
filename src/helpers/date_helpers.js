a37dffe42bc6be3e79c06d23f34b9f5d29b8e2b1
azwar <azwar.akbar@gmail.com>
2022-02-16 23:28:48 +0700

    update date calculation at date helper

--- src/helpers/date_helpers.js
+++ src/helpers/date_helpers.js
@@ -1,21 +1,31 @@
 const moment = require('moment-timezone');
+const FORMAT_AM_PM = "YYYY-MM-DD hh:mm a";
+const FORMAT_STD = "YYYY-MM-DD hh:mm";
 
 const timeUntil = (date, strTimeZone) => {
   console.log(date)
   console.log(strTimeZone)
 
-  const specialDay = moment.tz(date + ' 06:39 am', "YYYY-MM-DD h:mm a", strTimeZone)
-  const specialDayUtc = specialDay.utc()
-  const todayUtc = moment(new Date()).utc()
-  const age = todayUtc.diff(specialDayUtc, 'years');
-  let nextSpecialDay = moment(specialDay).add(age, 'years').utc();
-  let secondsRemaining = todayUtc.diff(nextSpecialDay, 'second')
+  const specialDayTz = moment.tz(date + ' 12:22 am', FORMAT_AM_PM, strTimeZone)
+  const todayUtc = moment().utc()
+  const strTodayUtc = todayUtc.format(FORMAT_STD)
+  const strSpecialDayTz = specialDayTz.format(FORMAT_STD)
+  const todayTz = moment.utc(strTodayUtc).tz(strTimeZone)
+  const strTodayUserTz = todayTz.format(FORMAT_STD)
+  const age = todayTz.diff(specialDayTz, 'years');
+  let nextSpecialDayTz = moment(strSpecialDayTz, FORMAT_STD, strTimeZone).add(age, 'years')
 
-  if (secondsRemaining < 0) {
-    nextSpecialDay.add(1, 'year')
+  const todayJsDate = moment(strTodayUserTz).toDate()
+  const nextSpecialDayJsDate = nextSpecialDayTz.toDate()
+  let minutesRemaining = moment(nextSpecialDayJsDate).diff(moment(todayJsDate), 'minutes');
+
+  if (minutesRemaining < 0) {
+    const nextTz2 = moment(nextSpecialDayJsDate).add(1, 'year')
+    const nextDt = nextTz2.toDate()
+    minutesRemaining = moment(nextDt).diff(moment(todayJsDate), 'minutes');
   }
 
-  return secondsRemaining;
+  return minutesRemaining * 60;
 }
 
 module.exports = {
